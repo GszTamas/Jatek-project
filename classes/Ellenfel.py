f0939f5ecb9ab80259121a5f32ec0c2b5dd5bc4b
Tamás <118980938+GszTamas@users.noreply.github.com>
2023-02-16 13:31:12 +0100

    Update Ellenfel.py

--- classes/Ellenfel.py
+++ classes/Ellenfel.py
@@ -1,5 +1,12 @@
 class Ellenfel:
+
     def __init__(self, name, HP, ugyesseg):
         self.name = name
         self.HP = HP
         self.ugyesseg = ugyesseg
+
+    def hpmod(self, szam):
+        if szam > 0:
+            self.HP = self.HP + szam
+        else:
+            self.HP = self.HP - szam
