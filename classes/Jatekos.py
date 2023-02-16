6c60bc0c8a9d9632279536a87f3addca4c17d0cb
Tamás <118980938+GszTamas@users.noreply.github.com>
2023-02-16 13:30:59 +0100

    Update Jatekos.py

--- classes/Jatekos.py
+++ classes/Jatekos.py
@@ -1,5 +1,13 @@
 class Jatekos:
-    def __init__(self, HP, Luck, Skill, Gold, Items=[], Crystals=[], Potions=[], Food=[]):
+    def __init__(self, HP, Luck, Skill, Gold, Items=None, Crystals=None, Potions=None, Food=None, lokacio = "0"):
+        if Food is None:
+            Food = []
+        if Potions is None:
+            Potions = []
+        if Crystals is None:
+            Crystals = []
+        if Items is None:
+            Items = []
         self.HP = HP
         self.Luck = Luck
         self.Skill = Skill
@@ -8,6 +16,7 @@ class Jatekos:
         self.Crystals = Crystals
         self.Potions = Potions
         self.Food = Food
+        self.lokacio = lokacio
 
     def tortenetkezdes(self):
         self.Items.append("Kard")
@@ -21,3 +30,9 @@ class Jatekos:
 
     def jatekosHeal(self, szam):
         self.HP = self.HP + szam
+
+    def lokaciovaltoztatas(self, szoba):
+        self.lokacio = szoba
+
+    def __repr__(self):
+        print(f'Életerőd: {self.HP}\nSzerencséd: {self.Luck}\nÜgyeséged: {self.Skill}\nHelyzeted: {int(self.lokacio)}\nPénzed: {self.Gold}\nTárgyaid: {self.Items}\nKristályaid: {self.Crystals}\nItalaid: {self.Potions}\nÉteleid: {self.Food}')
